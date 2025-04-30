4d596c179fc8e490ddf315b443009c60b4428586
Suelen Santos <suelensantos.cordeiro@gmail.com>
2025-04-30 13:15:17 +0000

    Add API root view to provide base URLs for API endpoints

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,6 +1,20 @@
+
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import User, Team, Activity, Workout, Leaderboard
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, WorkoutSerializer, LeaderboardSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = 'http://localhost:8000/'
+    return Response({
+        'users': base_url + 'api/users/?format=api',
+        'teams': base_url + 'api/teams/?format=api',
+        'activity': base_url + 'api/activity/?format=api',
+        'leaderboard': base_url + 'api/leaderboard/?format=api',
+        'workouts': base_url + 'api/workouts/?format=api',
+    })
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
